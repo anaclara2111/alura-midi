7170e4becd94ef2cb00a91b4ff22066e5f5f1e5b
anaclara2111 <106815907+anaclara2111@users.noreply.github.com>
2023-10-05 11:22:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,4 +8,13 @@ function tocaSom (seletorAudio) {
         //alert("elemento não encontrado");
         console.log("elemento não encontrado ou seletor inválido");
     }
+    const listaDeTeclas = document.querySelectorA11(".tecla");
+
+    //para
+    for (let contador = 0; < listaDeTeclas.lehgth; contador++) {
+
+        const tecla = listaDeTeclas[contador];
+        const instrumento = tecla.classList[1];
+        const idAudio = "#som_${instrumento}"; //template string
+    }
 }
